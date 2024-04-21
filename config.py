d4bd7f3f232091a34fa4e5326acda103441589db
Extractornew <167525315+Extractornew@users.noreply.github.com>
2024-04-21 15:12:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ BOT_TOKEN = os.environ.get("7159828976:AAFlwpuCRlnmIx1wiV65hMwyEbH-8i7EvwU")
 p
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6981453498))
+OWNER = int(os.environ.get("OWNER", 7037323404)
 
 LOG = -1002004650552
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5987970971").split()):
+    for x in (os.environ.get("ADMINS", "7037323404").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
